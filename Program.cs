bf6e30d1ce27f6132ae028e55c31fe8964101e35
Şevval Ensarioğlu <22290742@ogrenci.ankara.edu.tr>
2025-11-23 14:43:54 +0300

    duzenleme-2

--- Program.cs
+++ Program.cs
@@ -1,8 +1,51 @@
+using Microsoft.EntityFrameworkCore;
+using staj_forum_backend.Data;
+using staj_forum_backend.Data.Repositories;
+using staj_forum_backend.Mappings;
+using staj_forum_backend.Services;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+        options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+    });
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// Database Configuration
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// AutoMapper Configuration
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+// Repository Pattern - Register Repositories
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddScoped<IForumRepository, ForumRepository>();
+builder.Services.AddScoped<IContactRepository, ContactRepository>();
+
+// Service Layer - Register Services
+builder.Services.AddScoped<IForumService, ForumService>();
+builder.Services.AddScoped<IContactService, ContactService>();
+builder.Services.AddScoped<IChatService, ChatService>();
+
+// CORS Configuration
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173", "http://localhost:3000", "http://localhost:5174")
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials();
+    });
+});
 
 var app = builder.Build();
 
@@ -12,30 +55,13 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+// CORS middleware - UseCors must be called before UseAuthorization and MapControllers
+app.UseCors("AllowFrontend");
+
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseAuthorization();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapControllers();
 
 app.Run();
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
-
--- obj/Debug/net9.0/staj-forum-backend.AssemblyInfo.cs
+++ obj/Debug/net9.0/staj-forum-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("staj-forum-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25261e19f68199c97ecf3173f5cae776709cc4f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d83c66bb6ce2ff95080443f8ea161403bd561779")]
 [assembly: System.Reflection.AssemblyProductAttribute("staj-forum-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("staj-forum-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
